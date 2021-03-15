aabd8b19bb10ff14f0a316d76cdbb907fc8cbbfb
juanarmond <cuvviszonkax6mukTy>
2021-03-15 13:06:26 +0000

    redshift

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from aws_cdk import core
 from data_platform.data_lake.stack import DataLakeStack
+from data_platform.common_stack import CommonStack
+from data_platform.redshift.stack import RedshiftStack
 
 app = core.App()
 data_lake = DataLakeStack(app)
+common_stack = CommonStack(app)
+redshift = RedshiftStack(app, data_lake_raw=data_lake.data_lake_raw_bucket, data_lake_processed=data_lake.data_lake_processed_bucket, common_stack=common_stack)
 app.synth()
